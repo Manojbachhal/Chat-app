ee102c6cb4212fb2a78da5f7729da2ab79beec7d
manoj kumar <manojtewatia9211@gmail.com>
2024-09-10 14:30:38 +0530

    api url updated

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chat-app-clwu.onrender.com',
+  // apiUrl: 'https://chat-app-clwu.onrender.com',
+  apiUrl: 'https://server-tau-beige.vercel.app',
 };
